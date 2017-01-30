cd3e897241bb3d99f64badf8c699db2364f62fc4
johkal <johan.kallberg@ki.se>
2017-01-30 08:35:17 +0100

    SSFormHandler

--- src/org/jakz/common/SSFormHandler.java
+++ src/org/jakz/common/SSFormHandler.java
@@ -8,16 +8,20 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import org.jakz.common.Form.FieldType;
+
 public class SSFormHandler
 {
 	
 	//TODO use modified DataEntry? Merge DataEntry and Form
-	public static Form[] produceFormFromDBTable(String tableName, boolean withData, int dataLimit, Connection c) throws SQLException, ApplicationException
+	public static Form produceFormFromDBTable(String tableName, boolean withData, int dataLimit, Connection c) throws SQLException, ApplicationException
 	{
-		ArrayList<Form> rowForms = new ArrayList<Form>();
+		Form masterForm = new Form(tableName,FieldType.FORM);
 		
 		Statement s = c.createStatement();
-		ResultSet result; 
+		ResultSet result;
+		
+		//TODO change for more secure handling of arguments. Maybe use the query building package from TIEFighter?
 		if(withData)
 			result =  s.executeQuery("SELECT TOP "+dataLimit+" * FROM "+tableName);
 		else
@@ -26,10 +30,9 @@ public class SSFormHandler
 		ResultSetMetaData resultMeta = result.getMetaData();
 		for(int rowi=0; result.next(); rowi++)
 		{
-			Form rowForm = new Form(""+rowi,Form.FieldType.FORM);
+			Form rowForm = new Form(tableName+"_"+rowi,Form.FieldType.QUERY);
 			rowForm.name=""+rowi;
 			int colCount = resultMeta.getColumnCount();
-			System.out.println("col count = "+colCount);
 			for(int coli=1; coli<=colCount; coli++)  //index from 1
 			{
 				String columnName = resultMeta.getColumnName(coli);
@@ -41,48 +44,52 @@ public class SSFormHandler
 				Form columnForm = new Form(tableName+"."+columnName, Form.FieldType.QUERY);
 				TypedValue tv = new TypedValue(columnType);
 				
-				//dummy
-				result.getObject(coli);
-				if(!result.wasNull())
-				{	
-					if(columnType==java.sql.Types.BOOLEAN)
-					{
-						tv.setBoolean(result.getBoolean(coli));
+				if(withData)
+				{
+					//dummy
+					result.getObject(coli);
+					if(!result.wasNull())
+					{	
+						if(columnType==java.sql.Types.BOOLEAN)
+						{
+							tv.setBoolean(result.getBoolean(coli));
+						}
+						else if(columnType==java.sql.Types.TIMESTAMP)
+						{
+							tv.setTimestamp(result.getTimestamp(coli).getTime());
+						}
+						else if(columnType==java.sql.Types.BIGINT)
+						{
+							tv.setBigint(result.getLong(coli));
+						}
+						else if(columnType==java.sql.Types.INTEGER||columnType==java.sql.Types.SMALLINT)
+						{
+							tv.setInteger(result.getInt(coli));
+						}
+						else if(columnType==java.sql.Types.DOUBLE||columnType==java.sql.Types.DECIMAL)
+						{
+							tv.setDouble(result.getDouble(coli));
+						}
+						else if(columnType==java.sql.Types.VARCHAR)
+						{
+							tv.setVarchar(result.getString(coli));
+						}
+						else if(columnType==java.sql.Types.NVARCHAR||columnType==-16) //TODO why does SQL server give -16 for nvarchar(max)?
+						{
+							tv.setNvarchar(result.getString(coli));
+						}
+						else throw new ApplicationException("Could not parse Form value of table "+tableName+", column "+columnName+" with type "+columnType+" at relative line index "+rowi);
 					}
-					else if(columnType==java.sql.Types.TIMESTAMP)
-					{
-						tv.setTimestamp(result.getTimestamp(coli).getTime());
-					}
-					else if(columnType==java.sql.Types.BIGINT)
-					{
-						tv.setBigint(result.getLong(coli));
-					}
-					else if(columnType==java.sql.Types.INTEGER||columnType==java.sql.Types.SMALLINT)
-					{
-						tv.setInteger(result.getInt(coli));
-					}
-					else if(columnType==java.sql.Types.DOUBLE||columnType==java.sql.Types.DECIMAL)
-					{
-						tv.setDouble(result.getDouble(coli));
-					}
-					else if(columnType==java.sql.Types.VARCHAR)
-					{
-						tv.setVarchar(result.getString(coli));
-					}
-					else if(columnType==java.sql.Types.NVARCHAR||columnType==-16) //TODO why does SQL server give -16 for nvarchar(max)?
-					{
-						tv.setNvarchar(result.getString(coli));
-					}
-					else throw new ApplicationException("Could not parse Form value of table "+tableName+", column "+columnName+" with type "+columnType+" at relative line index "+rowi);
+					
+					columnForm.value.add(tv);
 				}
 				
-				columnForm.value.add(tv);
 				rowForm.addContent(columnForm);
 			}
 			
-			rowForms.add(rowForm);
+			masterForm.addContent(rowForm);
 		}
-		return rowForms.toArray(new Form[rowForms.size()]);
+		return masterForm;
 	}
 
 }
